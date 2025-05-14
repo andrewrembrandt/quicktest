e556c51ab008ee9cfda19bef69f0568e70784d09
Andrew Rembrandt <andrew.rembrandt@mimacom.com>
2025-05-14 10:53:46 +0200

    Initial commit

---
+++ src/main/java/com/mimacom/quicktest/QuicktestApplication.java
@@ -0,0 +1,13 @@
+package com.mimacom.quicktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicktestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuicktestApplication.class, args);
+	}
+
+}
