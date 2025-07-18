f2f77ea7bbb0a9f7e061a31450eb02f3f234bf35
Sascha Diefenthäler <sascha.diefenthaeler@mimacom.com>
2025-07-18 15:20:25 +0200

    feat: add todo item use case
    bare bones wip todo list add/read

--- src/main/java/com/mimacom/quicktest/QuicktestApplication.java
+++ src/main/java/com/mimacom/quicktest/QuicktestApplication.java
@@ -2,8 +2,10 @@ package com.mimacom.quicktest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class QuicktestApplication {
 
 	public static void main(String[] args) {
